f7ad936c65b4247b2218607116dfd7e8ea5c1e91
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-08 08:13:42 +0100

    shortened version

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const hamburgerIcon = document.querySelector('.fas.fa-bars');
-const closeBarsMenu = document.querySelector('.close');
-
-
-
-hamburgerIcon.addEventListener('click', function () {
-    hamburgerMenu.classList.toggle('active');
+document.querySelector(".fa-bars").addEventListener("click", function () {
+    document.querySelector(".hamburger-menu").classList.toggle("active");
 });
 
-closeBarsMenu.addEventListener('click', function () {
-    hamburgerMenu.classList.toggle('active');
-});
 
+
+document.querySelector(".fa-times").addEventListener("click", function () {
+    document.querySelector(".hamburger-menu").classList.toggle("active");
+}); 
 
 
 
